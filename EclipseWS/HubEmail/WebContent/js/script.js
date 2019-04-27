ebee551f53d3a305ba733444054ee7c671fae194
Lucas Romani <30930768+sacaia@users.noreply.github.com>
2019-04-27 17:35:04 -0300

    mt JQuery

--- EclipseWS/HubEmail/WebContent/js/script.js
+++ EclipseWS/HubEmail/WebContent/js/script.js
@@ -60,22 +60,134 @@ $(document).ready(function(){
         $(this).removeAttr("disabled");
         }
     });
-//////////////ON CLICK "CHECKBOX"////////////////////////////
+//////////////ON CLICK LIDA/NAO-LIDA////////////////////////////
     $("#lida-naoLida").click(function(){
         if($(this).hasClass("nao-lida"))
         {
             $(this).addClass("fa-envelope-open lida");
             $(this).removeClass("fa-envelope nao-lida");
+            $(this).attr({
+                    "data-original-title" : "Marcar como não lida"
+                });
         } 
         else
         {
             if($(this).hasClass("lida"))
             {
                 $(this).addClass("fa-envelope nao-lida");
-                $(this).removeClass("fa-envelope-open lida");                 
+                $(this).removeClass("fa-envelope-open lida");
+                $(this).attr({
+                    "data-original-title" : "Marcar como lida"
+                });
             }
-        }
-        
+        }    
     });
-
+//////////////ON CLICK PAG-PROX////////////////////////////
+    $("#pag-prox").click(function(){
+        if($("#pag3").hasClass("bg-selected"))
+        {
+            return;
+        }
+        if($("#pag2").hasClass("bg-selected"))
+        {
+            $("#pag2").removeClass("bg-selected");
+            $("#pag3").addClass("bg-selected");
+            $("#pag-prox-container").addClass("disabled");
+            return;
+        }
+        if($("#pag1").hasClass("bg-selected"))
+        {
+            $("#pag1").removeClass("bg-selected");
+            $("#pag2").addClass("bg-selected");
+            $("#pag-ant-container").removeClass("disabled");
+            return;
+        }
+    });
+//////////////ON CLICK PAG-ANT////////////////////////////
+    $("#pag-ant").click(function(){
+        if($("#pag1").hasClass("bg-selected"))
+        {
+            return;
+        }
+        if($("#pag2").hasClass("bg-selected"))
+        {
+            $("#pag2").removeClass("bg-selected");
+            $("#pag1").addClass("bg-selected");
+            $("#pag-ant-container").addClass("disabled");
+            return;
+        }
+        if($("#pag3").hasClass("bg-selected"))
+        {
+            $("#pag3").removeClass("bg-selected");
+            $("#pag2").addClass("bg-selected");
+            $("#pag-prox-container").removeClass("disabled");
+            return;
+        }
+    });
+/////////////////ON CLICK PAG1////////////////////////////
+    $("#pag1").click(function(){
+        if($(this).hasClass("bg-selected"))
+        {
+            return;
+        }
+        if($("#pag2").hasClass("bg-selected"))
+        {
+            $("#pag2").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-ant-container").addClass("disabled");
+            return;
+        }
+        if($("#pag3").hasClass("bg-selected"))
+        {
+            $("#pag3").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-ant-container").addClass("disabled");
+            $("#pag-prox-container").removeClass("disabled");
+            return;
+        }
+    });
+/////////////////ON CLICK PAG2////////////////////////////
+    $("#pag2").click(function(){
+        if($(this).hasClass("bg-selected"))
+        {
+            return;
+        }
+        if($("#pag1").hasClass("bg-selected"))
+        {
+            $("#pag1").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-ant-container").removeClass("disabled");
+            return;
+        }
+        if($("#pag3").hasClass("bg-selected"))
+        {
+            $("#pag3").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-prox-container").removeClass("disabled");
+            return;
+        }
+    })
+/////////////////ON CLICK PAG3////////////////////////////
+    $("#pag3").click(function(){
+        if($(this).hasClass("bg-selected"))
+        {
+            return;
+        }
+        if($("#pag1").hasClass("bg-selected"))
+        {
+            $("#pag1").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-prox-container").addClass("disabled");
+            $("#pag-ant-container").removeClass("disabled");
+            return;
+        }
+        if($("#pag2").hasClass("bg-selected"))
+        {
+            $("#pag2").removeClass("bg-selected");
+            $(this).addClass("bg-selected");
+            $("#pag-prox-container").addClass("disabled");
+            return;
+        }
+    });
+//////////////////////////////////////////////////////////
 });
